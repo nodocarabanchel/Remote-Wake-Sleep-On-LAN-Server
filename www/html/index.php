8dbbdb9b0230136441606c90d82aeca6db67340c
Joel <j.pocstl@gmail.com>
2024-04-16 16:47:49 +0200

    Update index.php

--- www/html/index.php
+++ www/html/index.php
@@ -7,13 +7,13 @@ Security Edits By: Felix Ryan (https://www.felixrr.pro)
 License: GPL v3 (http://www.gnu.org/licenses/gpl.html)
 */
 
-//You should not need to edit this file. Adjust Parameters in the config file:
+// Include configuration file
 require_once('config.php');
 
-//set headers that harden the HTTPS session
-if ($USE_HTTPS)
-{
-   header("Strict-Transport-Security: max-age=7776000"); //HSTS headers set for 90 days
+// Set headers to harden the HTTPS session
+if ($USE_HTTPS && !isset($_SERVER['HTTPS'])) {
+    header("Location: https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+    exit;
 }
 
 // Enable flushing
@@ -21,21 +21,13 @@ ini_set('implicit_flush', true);
 ob_implicit_flush(true);
 ob_end_flush();
 
-//Set the correct protocol
-if ($USE_HTTPS && !$_SERVER['HTTPS'])
-{
-   header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-   exit;
+// Set default computer (this is business logic so should be done last)
+if (empty($_GET)) {
+    header('Location: '. ($USE_HTTPS ? "https://" : "http://") . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . "?computer=0");
+    exit;
+} else {
+    $selectedComputerIndex = isset($_GET['computer']) ? intval($_GET['computer']) : 0; // Default to 0 if not provided or invalid
 }
-
-//Set default computer (this is business logic so should be done last)
-if (empty($_GET))
-{
-   header('Location: '. ($USE_HTTPS ? "https://" : "http://") . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . "?computer=0");
-   exit;
-}
-else
-   $_GET['computer'] = preg_replace("/[^0-9,.]/", "", $_GET['computer']);
 
 ?>
 
@@ -52,16 +44,16 @@ else
     <link href="<?php echo $BOOTSTRAP_LOCATION_PREFIX; ?>bootstrap/css/bootstrap.css" rel="stylesheet">
     <style type="text/css">
     body, html {
-        height: 100%; /* Ensure that the body takes up the full height of the viewport */
-        margin: 0; /* Remove default margin */
-        padding: 0; /* Remove default padding */
-        background: #f5f5f5; /* Optional: default background color */
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        background: #f5f5f5;
     }
     body {
-        background-image: url('bootstrap/background.png'); /* Adjust the path as necessary */
-        background-size: cover; /* Cover the entire screen */
-        background-position: center; /* Center the background image */
-        background-repeat: no-repeat; /* Do not repeat the image */
+        background-image: url('bootstrap/background.png');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
     }
     .form-signin {
         max-width: 600px;
@@ -72,8 +64,7 @@ else
         border-radius: 5px;
         box-shadow: 0 1px 2px rgba(0,0,0,.05);
     }
-</style>
-
+    </style>
 
     <link href="<?php echo $BOOTSTRAP_LOCATION_PREFIX; ?>bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 </head>
@@ -83,22 +74,22 @@ else
             <h2 class="form-signin-heading">Remote Wake NPC</h2>
             <?php
                 $approved = false;
-                $wake_up = false;
-                $go_to_sleep = false;
-                $check_current_status = false;
+                $wakeUp = false;
+                $goToSleep = false;
+                $checkCurrentStatus = false;
 
                 if (isset($_POST['password'])) {
                     if (!is_null($APPROVED_HASH) && password_verify($_POST['password'], $APPROVED_HASH)) {
                         $approved = true;
                         switch ($_POST['submitbutton']) {
                             case "Wake Up!":
-                                $wake_up = true;
+                                $wakeUp = true;
                                 break;
                             case "Sleep!":
-                                $go_to_sleep = true;
+                                $goToSleep = true;
                                 break;
                             case "Check Status":
-                                $check_current_status = true;
+                                $checkCurrentStatus = true;
                                 break;
                         }
                     }
@@ -107,72 +98,70 @@ else
                 if ($approved) {
                     echo "<select name='computer' onchange='this.form.submit()'>";
                     foreach ($COMPUTER_NAME as $index => $name) {
-                        echo "<option value='$index'" . ($selectedComputer == $index ? " selected" : "") . ">$name</option>";
+                        echo "<option value='$index'" . ($selectedComputerIndex == $index ? " selected" : "") . ">$name</option>";
                     }
                     echo "</select>";
                 } else {
                     echo "<p>Please log in.</p>";
                 }
 
-                if ($check_current_status) {
-    echo "<p>Approved. Please wait while the computer is queried for its current status...</p>";
-    $pinginfo = exec("ping -c 1 " . $COMPUTER_LOCAL_IP[$selectedComputer]);
-    if (empty($pinginfo)) {
-        echo "<p style='color:#CC0000;'><b>" . $COMPUTER_NAME[$selectedComputer] . " is currently offline.</b></p>";
-        echo "<form method='post'>";
-        echo "<input type='hidden' name='password' value='" . htmlspecialchars($_POST['password']) . "'>";
-        echo "<button type='submit' name='submitbutton' value='Wake Up!'>Wake Up!</button>";
-        echo "</form>";
-    } else {
-        echo "<p style='color:#00CC00;'><b>" . $COMPUTER_NAME[$selectedComputer] . " is currently online.</b></p>";
-    }
-} elseif ($wake_up) {
-   // Debugging: Print the value of $selectedComputer
-    echo "<p>Selected Computer: $selectedComputer</p>";
-   #/ Retrieve the destination IP address for wake-up operation
-    if (isset($selectedComputer) && isset($COMPUTER_LOCAL_IP[$selectedComputer])) {
-        $destinationAddress = $COMPUTER_LOCAL_IP[$selectedComputer];
-    } else {
-        echo "<p>Error: Selected computer is not valid.</p>";
-        // Handle the error condition accordingly
-        exit; // Or perform any other error handling
-    }
-    echo "<p>Approved. Sending WOL Command...</p>";
-    exec('wakeonlan ' . $COMPUTER_MAC[$selectedComputer]);
-    echo "<p>Command Sent. Waiting for " . $COMPUTER_NAME[$selectedComputer] . " to wake up...</p><p>";
-    $count = 1;
-    $down = true;
-    while ($count <= $MAX_PINGS && $down === true) {
-        echo "Ping " . $count . "...";
-        $pinginfo = exec("ping -c 1 " . $COMPUTER_LOCAL_IP[$selectedComputer]);
-        $count++;
-        if (!empty($pinginfo)) {
-            $down = false;
-            echo "<span style='color:#00CC00;'><b>It's Alive!</b></span><br />";
-            echo "<p><a href='?computer=" . $selectedComputer . "'>Return to the Wake/Sleep Control Home</a></p>";
-            $show_form = false;
-        } else {
-            echo "<span style='color:#CC0000;'><b>Still Down.</b></span><br />";
-        }
-        sleep($SLEEP_TIME);
-    }
-    if ($down === true) {
-        echo "<p style='color:#CC0000;'><b>FAILED!</b> " . $COMPUTER_NAME[$selectedComputer] . " doesn't seem to be waking up... Try again?</p>";
-        echo "<p>(Or <a href='?computer=" . $selectedComputer . "'>Return to the Wake/Sleep Control Home</a>.)</p>";
-    }
-} elseif ($go_to_sleep) {
-          echo "<p>Approved. Sending Sleep Command...</p>";
-          // Implement sleep command logic here
-          $sleepCommand = "ssh -o StrictHostKeyChecking=no " . $COMPUTER_USER[$selectedComputer] . "@" . $COMPUTER_LOCAL_IP[$selectedComputer] . " 'poweroff'";
-          exec($sleepCommand, $output, $return_var);
-          if ($return_var == 0) {
-              echo "<p style='color:#00CC00;'><b>Sleep command sent successfully.</b></p>";
-          } else {
-              echo "<p style='color:#CC0000;'><b>Failed to send sleep command.</b></p>";
-          }
-      } elseif (isset($_POST['submitbutton'])) {
-    echo "<p style='color:#CC0000;'><b>Invalid Passphrase. Request Denied.</b></p>";
-}
+                if ($checkCurrentStatus) {
+                    echo "<p>Approved. Please wait while the computer is queried for its current status...</p>";
+                    $pingInfo = exec("ping -c 1 " . $COMPUTER_LOCAL_IP[$selectedComputerIndex]);
+                    if (empty($pingInfo)) {
+                        echo "<p style='color:#CC0000;'><b>" . $COMPUTER_NAME[$selectedComputerIndex] . " is currently offline.</b></p>";
+                        echo "<form method='post'>";
+                        echo "<input type='hidden' name='password' value='" . htmlspecialchars($_POST['password']) . "'>";
+                        echo "<button type='submit' name='submitbutton' value='Wake Up!'>Wake Up!</button>";
+                        echo "</form>";
+                    } else {
+                        echo "<p style='color:#00CC00;'><b>" . $COMPUTER_NAME[$selectedComputerIndex] . " is currently online.</b></p>";
+                    }
+                } elseif ($wakeUp) {
+                    echo "<p>Selected Computer: $selectedComputerIndex</p>";
+                    if (isset($selectedComputerIndex) && isset($COMPUTER_LOCAL_IP[$selectedComputerIndex])) {
+                        $destinationAddress = $COMPUTER_LOCAL_IP[$selectedComputerIndex];
+                    } else {
+                        echo "<p>Error: Selected computer is not valid.</p>";
+                        exit;
+                    }
+                    echo "<p>Approved. Sending WOL Command...</p>";
+                    exec('wakeonlan ' . $COMPUTER_MAC[$selectedComputerIndex]);
+                    echo "<p>Command Sent. Waiting for " . $COMPUTER_NAME[$selectedComputerIndex] . " to wake up...</p><p>";
+                    $count = 1;
+                    $down = true;
+                    while ($count <= $MAX_PINGS && $down === true) {
+                        echo "Ping " . $count . "...";
+                        $pingInfo = exec("ping -c 1 " . $COMPUTER_LOCAL_IP[$selectedComputerIndex]);
+                        $count++;
+                        if (!empty($pingInfo)) {
+                            $down = false;
+                            echo "<span style='color:#00CC00;'><b>It's Alive!</b></span><br />";
+                            echo "<p><a href='?computer=" . $selectedComputerIndex . "'>Return to the Wake/Sleep Control Home</a></p>";
+                            $showForm = false;
+                        } else {
+                            echo "<span style='color:#CC0000;'><b>Still Down.</b></span><br />";
+                        }
+                        sleep($SLEEP_TIME);
+                    }
+                    if ($down === true) {
+                        echo "<p style='color:#CC0000;'><b>FAILED!</b> " . $COMPUTER_NAME[$selectedComputerIndex] . " doesn't seem to be waking up... Try again?</p>";
+                        echo "<p>(Or <a href='?computer=" . $selectedComputerIndex . "'>Return to the Wake/Sleep Control Home</a>.)</p>";
+                    }
+                } elseif ($goToSleep) {
+                    echo "<p>Approved. Sending Sleep Command...</p>";
+                    // Implement sleep command logic here
+                    $sleepCommand = "ssh -o StrictHostKeyChecking=no " . $COMPUTER_USER[$selectedComputerIndex] . "@" . $COMPUTER_LOCAL_IP[$selectedComputerIndex] . " 'poweroff'";
+                    exec($sleepCommand, $output, $returnVar);
+                    if ($returnVar == 0) {
+                        echo "<p style='color:#00CC00;'><b>Sleep command sent successfully.</b></p>";
+                    } else {
+                        echo "<p style='color:#CC0000;'><b>Failed to send sleep command.</b></p>";
+                    }
+                } elseif (isset($_POST['submitbutton'])) {
+                    echo "<p style='color:#CC0000;'><b>Invalid Passphrase. Request Denied.</b></p>";
+                }
+                
                 if (!isset($_POST['submitbutton']) || !$approved) {
                     echo '<input type="password" name="password" class="input-block-level" placeholder="Enter Passphrase">';
                     echo '<button class="btn btn-large btn-primary" type="submit" name="submitbutton" value="Check Status">Check Status</button>';
