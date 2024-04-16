d5157c3b1e2f26db384241156fb4f8fe23392b85
Joel <j.pocstl@gmail.com>
2024-04-16 15:50:36 +0200

    Update index.php

--- www/html/index.php
+++ www/html/index.php
@@ -112,24 +112,27 @@ else
 
                 if ($check_current_status) {
     echo "<p>Approved. Please wait while the computer is queried for its current status...</p>";
-    // Implement status check logic here
     $pinginfo = exec("ping -c 1 " . $COMPUTER_LOCAL_IP[$selectedComputer]);
     if (empty($pinginfo)) {
         echo "<p style='color:#CC0000;'><b>" . $COMPUTER_NAME[$selectedComputer] . " is currently offline.</b></p>";
+        // Provide a button to wake up the computer
+        echo "<form method='post'>";
+        echo "<input type='hidden' name='password' value='" . htmlspecialchars($_POST['password']) . "'>";
+        echo "<button type='submit' name='submitbutton' value='Wake Up!'>Wake Up!</button>";
+        echo "</form>";
     } else {
         echo "<p style='color:#00CC00;'><b>" . $COMPUTER_NAME[$selectedComputer] . " is currently online.</b></p>";
     }
-      } elseif ($wake_up) {
-          echo "<p>Approved. Sending WOL Command...</p>";
-          // Implement wake command logic here
-          $wakeCommand = "wakeonlan " . $COMPUTER_MAC[$selectedComputer];
-          exec($wakeCommand, $output, $return_var);
-          if ($return_var == 0) {
-              echo "<p style='color:#00CC00;'><b>WOL command sent successfully.</b></p>";
-          } else {
-              echo "<p style='color:#CC0000;'><b>Failed to send WOL command.</b></p>";
-          }
-      } elseif ($go_to_sleep) {
+} elseif ($wake_up) {
+    echo "<p>Approved. Sending WOL Command...</p>";
+    $wakeCommand = "wakeonlan " . $COMPUTER_MAC[$selectedComputer];
+    exec($wakeCommand, $output, $return_var);
+    if ($return_var == 0) {
+        echo "<p style='color:#00CC00;'><b>WOL command sent successfully.</b></p>";
+    } else {
+        echo "<p style='color:#CC0000;'><b>Failed to send WOL command.</b></p>";
+    }
+} elseif ($go_to_sleep) {
           echo "<p>Approved. Sending Sleep Command...</p>";
           // Implement sleep command logic here
           $sleepCommand = "ssh -o StrictHostKeyChecking=no " . $COMPUTER_USER[$selectedComputer] . "@" . $COMPUTER_LOCAL_IP[$selectedComputer] . " 'poweroff'";
@@ -140,9 +143,8 @@ else
               echo "<p style='color:#CC0000;'><b>Failed to send sleep command.</b></p>";
           }
       } elseif (isset($_POST['submitbutton'])) {
-          echo "<p style='color:#CC0000;'><b>Invalid Passphrase. Request Denied.</b></p>";
-      }
-
+    echo "<p style='color:#CC0000;'><b>Invalid Passphrase. Request Denied.</b></p>";
+}
                 if (!isset($_POST['submitbutton']) || !$approved) {
                     echo '<input type="password" name="password" class="input-block-level" placeholder="Enter Passphrase">';
                     echo '<button class="btn btn-large btn-primary" type="submit" name="submitbutton" value="Check Status">Check Status</button>';
